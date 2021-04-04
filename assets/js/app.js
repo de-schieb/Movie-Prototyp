ba1e008c816aff7ac1444334077211187a9cddf0
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-04 16:16:07 +0200

    removed some unnecessary code

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,133 +4,11 @@ const log = console.log;
 
 // Selecting elements from the DOM
 const searchButton = document.querySelector('#search');
-const searchInput = document.querySelector('#exampleInputEmail1');
 const reservationPopup = document.querySelector('#reservation-popup');
 const bodyContainer = document.querySelector('#body-container');
-// const moviesContainer = document.querySelector('#movies-container');
-// const moviesSearchable = document.querySelector('#movies-searchable');
-
-function createImageContainer(imageUrl, id) {
-    const tempDiv = document.createElement('div');
-    tempDiv.setAttribute('class', 'imageContainer');
-    tempDiv.setAttribute('data-id', id);
-
-    const movieElement = `
-        <img src="${imageUrl}" alt="" data-movie-id="${id}">
-    `;
-    tempDiv.innerHTML = movieElement;
-
-    return tempDiv;
-}
 
 function resetInput() {
     searchInput.value = '';
-}
-
-function handleGeneralError(error) {
-    log('Error: ', error.message);
-    alert(error.message || 'Internal Server');
-}
-
-function createIframe(video) {
-    const videoKey = (video && video.key) || 'No key found!!!';
-    const iframe = document.createElement('iframe');
-    iframe.src = `http://www.youtube.com/embed/${videoKey}`;
-    iframe.width = 360;
-    iframe.height = 315;
-    iframe.allowFullscreen = true;
-    return iframe;
-}
-
-function insertIframeIntoContent(video, content) {
-    const videoContent = document.createElement('div');
-    const iframe = createIframe(video);
-
-    videoContent.appendChild(iframe);
-    content.appendChild(videoContent);
-}
-
-function createVideoTemplate(data) {
-    const content = this.content;
-    content.innerHTML = '<p id="content-close">X</p>';
-    
-    const videos = data.results || [];
-
-    if (videos.length === 0) {
-        content.innerHTML = `
-            <p id="content-close">X</p>
-            <p>No Trailer found for this video id of ${data.id}</p>
-        `;
-        return;
-    }
-
-    for (let i = 0; i < 4; i++) {
-        const video = videos[i];
-        insertIframeIntoContent(video, content);
-    }
-}
-
-function createSectionHeader(title) {
-    const header = document.createElement('h2');
-    header.innerHTML = title;
-
-    return header;
-}
-
-
-function renderMovies(data) {
-    const moviesBlock = generateMoviesBlock(data);
-    const header = createSectionHeader(this.title);
-    moviesBlock.insertBefore(header, moviesBlock.firstChild);
-    // moviesContainer.appendChild(moviesBlock);
-}
-
-
-
-// function renderSearchMovies(data) {
-//     moviesSearchable.innerHTML = '';
-//     const moviesBlock = generateMoviesBlock(data);
-//     moviesSearchable.appendChild(moviesBlock);
-// }
-
-
-function generateMoviesBlock(data) {
-    const movies = data.results;
-    const section = document.createElement('section');
-    section.setAttribute('class', 'section');
-
-    for (let i = 0; i < movies.length; i++) {
-        const { poster_path, id, release_date} = movies[i];
-        log("log: " + release_date);
-        if (poster_path&&(/^2019|^2020|^2021/.test(release_date))) {
-            // log("provider: " + getProviderOfMovie(id));
-            log("log = cinema movie: " + release_date);
-            //TODO: check how to use watch/provier property (or method ???)
-            const imageUrl = MOVIE_DB_IMAGE_ENDPOINT + poster_path;
-    
-            const imageContainer = createImageContainer(imageUrl, id);
-            section.appendChild(imageContainer);
-        }
-    }
-
-    const movieSectionAndContent = createMovieContainer(section);
-    return movieSectionAndContent;
-}
-
-// Inserting section before content element
-function createMovieContainer(section) {
-    const movieElement = document.createElement('div');
-    movieElement.setAttribute('class', 'movie');
-
-    const template = `
-        <div class="content">
-            <p id="content-close">X</p>
-        </div>
-    `;
-
-    movieElement.innerHTML = template;
-    movieElement.insertBefore(section, movieElement.firstChild);
-    return movieElement;
 }
 
 searchButton.onclick = function (event) {
@@ -183,11 +61,3 @@ document.onclick = function (event) {
         content.classList.remove('content-display');
     }
 }
-  
-
-// Initialize the search
-searchMovie(INITIAL_SEARCH_VALUE);
-// searchUpcomingMovies();
-// searchTopRatedMovies();
-// searchPopularMovie();
-// searchTrendingMovies();
