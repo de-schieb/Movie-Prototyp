ab25b453db4b25b6a74911561a9f9d1f14e1c59a
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-13 22:31:53 +0200

    fix

--- assets/js/apiCalls.js
+++ assets/js/apiCalls.js
@@ -1,17 +1,27 @@
 
-const apiUrl = 'cinema-68.germanywestcentral.cloudapp.azure.com:8090'
-const movieIDs = [1000,1001,1002,1003,1004,1005,1006,1007,1008,1010]
+const API_URL = 'http://cinema-68.germanywestcentral.cloudapp.azure.com:8090'
+const movieIDs = [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1010]
 
 function doFetch(url){
     var data = fetch(url)
-    .then(response => response.json)
-    .catch(error => console.error(error));
+    .then(response => response.text)
+    .catch(handleGeneralError);
     return data;
+}
+
+function generateUrl(path){
+    const url = `${API_URL}/${path}`;
+    return url;
+}
+
+function handleGeneralError(error) {
+    log('Error: ', error.message);
+    alert(error.message || 'Internal Server');
 }
 
 function getPicturePathByMovieID(){
     for(var i = 0;i<movieIDs.length;i++){
-        var picturePath = doFetch(apiUrl + "/picturePathByMovieID/" + movieIDs[i]);
+        var picturePath = doFetch(generateUrl("picturePathByMovieID/" + movieIDs[i]));
         console.log(picturePath);
         document.getElementById("movie_img_" + movieIDs[i]).src=picturePath;
     }
