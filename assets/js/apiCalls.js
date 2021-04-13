2a59945a08f42b5af3779b803fec0c6191dd21e9
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-13 22:59:04 +0200

    fix

--- assets/js/apiCalls.js
+++ assets/js/apiCalls.js
@@ -4,7 +4,7 @@ const movieIDs = [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1010]
 
 function doFetch(url){
     var data = fetch(url)
-    .then(response => response.url)
+    .then(response => response.data)
     -then(data => console.log("data: " + data))
     .catch(handleGeneralError);
     return data;
