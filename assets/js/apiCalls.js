831464458709ebb019aa547712ea8ad2494ce17c
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-14 14:04:13 +0200

    fix

--- assets/js/apiCalls.js
+++ assets/js/apiCalls.js
@@ -26,17 +26,16 @@ function handleGeneralError(error) {
 
 async function getPicturePathByMovieID(){
     for(var i = 0; i < movieIDs.length; i++){
-        // var url = generateUrl(`picturePathByMovieID/` + "1000");
         var url = generateUrl(`picturePathByMovieID/` + movieIDs[i]);
         console.log("url: " + url);
         var picturePath = (await doFetch(url));
         console.log("picturePath: " + picturePath);
-        var img = document.createElement("img");
-        img.setAttribute("class", "movie_img");
-        img.setAttribute("id", "movie_img_" + movieIDs[i]);
-        img.setAttribute("src", picturePath);
-        document.getElementById("body-container").appendChild(img);
-        // document.getElementById("movie_img" + movieIDs[i]).src = data;
+        // var img = document.createElement("img");
+        // img.setAttribute("class", "movie_img");
+        // img.setAttribute("id", "movie_img_" + movieIDs[i]);
+        // img.setAttribute("src", picturePath);
+        // document.getElementById("body-container").appendChild(img);
+        document.getElementById("movie_img" + movieIDs[i]).src = picturePath;
     }
 }    
 
