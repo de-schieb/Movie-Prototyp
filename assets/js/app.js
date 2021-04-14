6b2f200f9370adc350bec0a45ba070447628682e
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-14 18:09:17 +0200

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,7 +35,7 @@ function openMoviePopup(div) {
     console.log("movie_id: " + movie_id);
     var picture_path = div.src.substring(55);
     console.log("picture_path: " + picture_path);
-    document.getElementById("movie_img_big").setAttribute("src", picture_path);
+    // document.getElementById("movie_img_big").setAttribute("src", picture_path);
     moviePopupMain.style.display = "grid";
     moviePopupDetails.style.display = "grid";
     moviePopupButtons.style.display = "grid";
