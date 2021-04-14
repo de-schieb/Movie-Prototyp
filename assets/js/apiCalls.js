557ee2970d00650f7db229205149377f5c8a8ba6
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-14 08:54:12 +0200

    fix

--- assets/js/apiCalls.js
+++ assets/js/apiCalls.js
@@ -3,10 +3,17 @@ const API_URL = 'http://cinema-68.germanywestcentral.cloudapp.azure.com:8090'
 const movieIDs = [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1010] 
 
 function doFetch(url){
-    return fetch(url)
-    .then((res) => res.text())
-    .then((data) => console.log(data))    
-    .catch(handleGeneralError);
+    return $.ajax({ "async": true,
+    "crossDomain": true,
+    "url": url,
+    "method": "GET"
+  }).done(function (data){
+      return data;
+  })
+    // fetch(url)
+    // .then((res) => res.text())
+    // .then((data) => console.log(data))    
+    // .catch(handleGeneralError);
 }
 
 function generateUrl(path){
