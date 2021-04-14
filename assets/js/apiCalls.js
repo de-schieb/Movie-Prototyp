020f88948c033de0c3790bcb1fbf961443f6bbb1
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-14 08:47:34 +0200

    fix

--- assets/js/apiCalls.js
+++ assets/js/apiCalls.js
@@ -5,6 +5,7 @@ const movieIDs = [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1010]
 function doFetch(url){
     return fetch(url)
     .then((res) => res.text())
+    .then((data) => console.log(data))    
     .catch(handleGeneralError);
 }
 
