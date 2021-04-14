e1934b75740517a2447d387ed2fc2abda7ad1ee0
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-14 09:25:40 +0200

    fix

--- assets/js/apiCalls.js
+++ assets/js/apiCalls.js
@@ -6,7 +6,7 @@ function doFetch(url,onComplete){
     return fetch(url)
     .then((res) => res.text())
     .then((data) => console.log("data: " + data))
-    .then((data) => onComplete(data))
+    .then(onComplete)
     .catch(handleGeneralError);
 }
 
@@ -21,8 +21,9 @@ function handleGeneralError(error) {
 }
 
 function setPicturePath(data){
-    console.log("picturePath: " + data);    
-    document.getElementById("movie_img_" + movieIDs[i]).src=data;
+    const picturePath = data;
+    console.log("picturePath: " + picturePath);    
+    document.getElementById("movie_img_" + movieIDs[i]).src=picturePath;
 }
 
 function getPicturePathByMovieID(){
