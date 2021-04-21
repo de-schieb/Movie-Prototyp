120eae3fb11b704c29fa8bcf1c39a2c3e83a2472
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-21 09:00:27 +0200

    testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -56,10 +56,9 @@ function aboutUs(){
         type: 'info',
         icon: 'info',
         iconColor: "#000000",
-        confirmButtonText: 'Alles klar!',
+        closeButtonHtml: "<button class=\"btn\">Alles klar!</button>",
         buttonsStyling: false,
         timer: 35000,
-        timerProgressBar: true,
     })
 }
 
