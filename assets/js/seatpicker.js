267f8cc424e873dbd7e0a4db7f7ce60babb9f4e7
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-24 17:57:22 +0200

    testing

--- assets/js/seatpicker.js
+++ assets/js/seatpicker.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('#seatplan-container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const allSeats = document.querySelectorAll(".row .seat");
+const pickedSeats = document.getElementById("seats-seatplan");
 const total = document.getElementById('total-price-seatplan');
 
 let ticketPrice = 8;
@@ -8,19 +9,8 @@ let ticketPrice = 8;
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
-
-  // Copy selected seat into an array 
-  // Map through the array
-  // Return a new array indexes
-
-//   const seatsIndex = [...selectedSeats].map((seat) =>{
-//     return [...seats].indexOf(seat)
-//   })
-
-//   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-
   const selectedSeatsCount = selectedSeats.length;
-
+  pickedSeats = selectedSeats.id.join(", ");
   total.innerText = (selectedSeatsCount * ticketPrice) + "€";
 }
 
@@ -73,15 +63,6 @@ function populateUI(fetchedSeatPlanPattern) {
             document.getElementById(seatPlanPatternMixed[i].seatIDFE).classList.toggle('occupied');
         }
     }
-    // const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
-
-    // if (selectedSeats !== null && selectedSeats.length > 0) {
-    //     seats.forEach((seat, index) => {
-    //         if (selectedSeats.indexOf(index) > -1) {
-    //         seat.classList.add('selected');
-    //         }
-    //     });
-    // }
 }
 
 function cleanUI(){
@@ -98,15 +79,12 @@ function cleanUI(){
 }
 
 
-// Seat click event
 container.addEventListener('click', (e) => {
   if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
     e.target.classList.toggle('selected');
-
     updateSelectedCount()
   }
 })
 
 // Initial count and total set 
-
 updateSelectedCount();
