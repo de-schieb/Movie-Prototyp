57ccb737a2297f050019d653df6812882bd756a9
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-24 16:37:52 +0200

    testing

--- assets/js/seatpicker.js
+++ assets/js/seatpicker.js
@@ -68,7 +68,7 @@ function populateUI(fetchedSeatPlanPattern) {
     let seatPlanPatternMixed = populateMixedSeatPlanPattern(seatPlanPatternFrontend,seatPlanPatternDatabase);
 
     for(let i = 0;i<seatPlanPatternMixed.length;i++){
-        if(seatPlanPatternMixed[i].seatFree === false){
+        if(!Boolean(seatPlanPatternMixed[i].seatFree)){
             console.log("IN!!!!");
             document.getElementById(seatPlanPatternMixed[i].seatIDFE).classList.toggle('occupied');
         }
